de476533373692bb6838d00f63962e7d81c9a8c3
Ramya <ramyasrimuthineni12@gmail.com>
2017-06-29 17:22:44 -0500

    Angular2- NgFor directive usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl : 'app/app.componentngif.html',
+  templateUrl : 'app/app.component2.html',
 })
 export class AppComponent  {
   appTitle: string = 'Welcome';
-  appStatus : boolean = true;
+   appList: any[] = [ {
+      "ID": "1",
+      "Name" : "One"
+   },
+
+   {
+      "ID": "2",
+      "Name" : "Two"
+   } ];
  }
 
 
