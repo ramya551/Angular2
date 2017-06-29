8d367226c5b5cedef118d8a95ca8391e94609404
Ramya <ramyasrimuthineni12@gmail.com>
2017-06-29 13:41:25 -0500

    Angular2- Component declaration using templete-url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl : 'app/app.component.html',
 })
-export class AppComponent  { name = 'Angular22'; }
+export class AppComponent  {
+  appTitle: string = 'Welcome';
+ }
+
+
+
+
+
